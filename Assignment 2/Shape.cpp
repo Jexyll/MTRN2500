8a96495f2d42533df489567a9a6a4b2d7c47fe77
Jexyll <42834771+Jexyll@users.noreply.github.com>
2018-08-31 21:20:39 +0930

    Add files via upload
    Example from line 212 in main

--- Assignment 2/Shape.cpp
+++ Assignment 2/Shape.cpp
@@ -119,41 +119,54 @@ void Shape::setColor(float red_, float green_, float blue_) {
 //Draw command for a rectangular prism, rotation is about the initial point
 void rectangularprism::draw() {
 	//convert degrees to radians and find cosine and sine
-	double yrc = cos(3.141592765*rotation / 180);
-	double yrs = sin(3.141592765*rotation / 180);
+		double rc = cos(3.141592765*rotation / 180);
+		double rs = sin(3.141592765*rotation / 180);
 	//define all vertexes
-	double x1 = x;
-	double x2 = x1 + xlength * yrc;
-	double x3 = x1 - zlength * yrs;
-	double x4 = (x2 + x3) - x1;
-	double y1 = y;
-	double y2 = y1 + ylength;
-	double z1 = z;
-	double z2 = z1 + xlength * yrs;
-	double z3 = z1 + zlength * yrc;
-	double z4 = z2 + z3 - z1;
-	glColor3d(red, green, blue);
+	xp[0] = x;
+	xp[1] = xp[0] + xlength * rc;
+	xp[3] = xp[0] - zlength * rs;
+	xp[2] = (xp[1] + xp[3]) - xp[0];
+	xp[4] = xp[0];
+	xp[5] = xp[1];
+	xp[6] = xp[2];
+	xp[7] = xp[3];
+	yp[0] = y;
+	yp[1] = yp[0];
+	yp[2] = yp[0];
+	yp[3] = yp[0];
+	yp[4] = yp[0] + ylength;
+	yp[5] = yp[0] + ylength;
+	yp[6] = yp[0] + ylength;
+	yp[7] = yp[0] + ylength;
+	zp[0] = z;
+	zp[1] = zp[0] + xlength * rs;
+	zp[3] = zp[0] + zlength * rc;
+	zp[2] = zp[1] + zp[3] - zp[0];
+	zp[4] = zp[0];
+	zp[5] = zp[1];
+	zp[6] = zp[2];
+	zp[7] = zp[3];
 	glBegin(GL_QUAD_STRIP);
-	glVertex3d(x1, y1, z1);
-	glVertex3d(x2, y1, z2);
-	glVertex3d(x3, y1, z3);
-	glVertex3d(x4, y1, z4);
-	glVertex3d(x3, y2, z3);
-	glVertex3d(x4, y2, z4);
-	glVertex3d(x1, y2, z1);
-	glVertex3d(x2, y2, z2);
-	glVertex3d(x1, y1, z1);
-	glVertex3d(x2, y2, z2);
+	glVertex3d(xp[0], yp[0], zp[0]);
+	glVertex3d(xp[1], yp[1], zp[1]);
+	glVertex3d(xp[3], yp[3], zp[3]);
+	glVertex3d(xp[2], yp[2], zp[2]);
+	glVertex3d(xp[7], yp[7], zp[7]);
+	glVertex3d(xp[6], yp[6], zp[6]);
+	glVertex3d(xp[4], yp[4], zp[4]);
+	glVertex3d(xp[5], yp[5], zp[5]);
+	glVertex3d(xp[0], yp[0], zp[0]);
+	glVertex3d(xp[1], yp[1], zp[1]);
 	glEnd();
 	glBegin(GL_QUADS);
-	glVertex3d(x1, y1, z1);
-	glVertex3d(x3, y1, z3);
-	glVertex3d(x3, y2, z3);
-	glVertex3d(x1, y2, z1);
-	glVertex3d(x2, y1, z2);
-	glVertex3d(x4, y1, z4);
-	glVertex3d(x4, y2, z4);
-	glVertex3d(x2, y2, z2);
+	glVertex3d(xp[0], yp[0], zp[0]);
+	glVertex3d(xp[3], yp[3], zp[3]);
+	glVertex3d(xp[7], yp[7], zp[7]);
+	glVertex3d(xp[5], yp[5], zp[5]);
+	glVertex3d(xp[1], yp[1], zp[1]);
+	glVertex3d(xp[2], yp[2], zp[2]);
+	glVertex3d(xp[6], yp[6], zp[6]);
+	glVertex3d(xp[4], yp[4], zp[4]);
 	glEnd();
 }
 //dimension settings for a rectangular prism
@@ -162,3 +175,125 @@ void rectangularprism::setdimensions(double xlength_, double ylength_, double zl
 	ylength = ylength_;
 	zlength = zlength_;
 }
+//dimension settings for a triangular prism
+void triangularprism::setdimensions(double base_, double height_, double offset_, double length_) {
+	base = base_;
+	height = height_;
+	length = length_;
+	offset = offset_;
+
+}
+void triangularprism::draw() {
+	double rc = cos(3.141592765*rotation / 180);
+	double rs = sin(3.141592765*rotation / 180);
+	double x1 = x;
+	double y1 = y;
+	double y2 = y + height;
+	double x2 = x1 + base * rc;
+	double x3 = x1 + offset * rc;
+	double z1 = z;
+	double z2 = z1 + base * rs;
+	double z3 = z1 + offset * rs;
+	double x4 = x1 - length * rs;
+	double z4 = z1 + length * rc;
+	double x5 = x2 - length * rs;
+	double z5 = z2 + length * rc;
+	double x6 = x3 - length * rs;
+	double z6 = z3 + length * rc;
+	glBegin(GL_POLYGON);
+	glVertex3d(x1, y1, z1);
+	glVertex3d(x2, y1, z2);
+	glVertex3d(x3, y2, z3);
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex3d(x4, y1, z4);
+	glVertex3d(x5, y1, z5);
+	glVertex3d(x6, y2, z6);
+	glEnd();
+	glBegin(GL_QUAD_STRIP);
+	glVertex3d(x1, y1, z1);
+	glVertex3d(x4, y1, z4);
+	glVertex3d(x2, y1, z2);
+	glVertex3d(x5, y1, z5);
+	glVertex3d(x3, y2, z3);
+	glVertex3d(x6, y2, z6);
+	glVertex3d(x1, y1, z1);
+	glVertex3d(x4, y1, z4);
+	glEnd();
+
+}
+
+//dimension settings for a trapezoidal prism
+void trapezoid::setdimensions(double base_, double height_, double offset_, double length_, double topwidth_) {
+	base = base_;
+	height = height_;
+	length = length_;
+	offset = offset_;
+	topwidth = topwidth_;
+}
+//draw a trapezoidal prism
+void trapezoid::draw() {
+
+	double rc = cos(3.141592765*rotation / 180);
+	double rs = sin(3.141592765*rotation / 180);
+	xp[0] = x;
+	yp[0] = y;
+	yp[1] = y;
+	yp[2] = y + height;
+	yp[3] = y + height;
+	yp[4] = y;
+	yp[5] = y;
+	yp[6] = y + height;
+	yp[7] = y + height;
+	xp[1] = xp[0] + base * rc;
+	xp[2] = xp[0] + (offset + topwidth)*rc;
+	xp[3] = xp[0] + offset * rc;
+	zp[0] = z;
+	zp[1] = zp[0] + base * rs;
+	zp[2] = zp[0] + (offset + topwidth)*rs;
+	zp[3] = zp[0] + offset * rs;
+	xp[4] = xp[0] - length * rs;
+	zp[4] = zp[0] + length * rc;
+	xp[5] = xp[1] - length * rs;
+	zp[5] = zp[1] + length * rc;
+	xp[6] = xp[2] - length * rs;
+	zp[6] = zp[2] + length * rc;
+	xp[7] = xp[3] - length * rs;
+	zp[7] = zp[3] + length * rc;
+
+	glBegin(GL_POLYGON);
+	glVertex3d(xp[0], yp[0], zp[0]);
+	glVertex3d(xp[1], yp[1], zp[1]);
+	glVertex3d(xp[2], yp[2], zp[2]);
+	glVertex3d(xp[3], yp[3], zp[3]);
+
+	glEnd();
+	glBegin(GL_POLYGON);
+	glVertex3d(xp[4], yp[4], zp[4]);
+	glVertex3d(xp[5], yp[5], zp[5]);
+	glVertex3d(xp[6], yp[6], zp[6]);
+	glVertex3d(xp[7], yp[7], zp[7]);
+
+	glEnd();
+	glBegin(GL_QUAD_STRIP);
+	glVertex3d(xp[0], yp[0], zp[0]);
+	glVertex3d(xp[4], yp[4], zp[4]);
+	glVertex3d(xp[1], yp[1], zp[1]);
+	glVertex3d(xp[5], yp[5], zp[5]);
+	glVertex3d(xp[2], yp[2], zp[2]);
+	glVertex3d(xp[6], yp[6], zp[6]);
+	glVertex3d(xp[3], yp[3], zp[3]);
+	glVertex3d(xp[7], yp[7], zp[7]);
+	glVertex3d(xp[0], yp[0], zp[0]);
+	glVertex3d(xp[4], yp[4], zp[4]);
+	glEnd();
+
+}
+//grab a point from a trapezoidal prism
+/* obsolete, see Shape.hpp for reasoning
+void Shape::getpoint(int i) {
+	xpoint = xp[i];
+	ypoint = yp[i];
+	zpoint = zp[i];
+}
+*/
--- Assignment 2/Shape.hpp
+++ Assignment 2/Shape.hpp
@@ -27,10 +27,17 @@ public:
 	void positionInGL();
 	void setColorInGL();
 
+	//void getpoint(int i); only necessary if xp,yp,zp are private/protected
+	double xp[8] = { 0 }; // max number of points in a 3d shape (for our purposes) is 8
+	double yp[8] = { 0 };
+	double zp[8] = { 0 };
+
 	double getRed();
 	double getGreen();
 	double getBlue();
 	void setColor(float red_, float green_, float blue_);
+
+	//double xpoint, ypoint, zpoint; as above, unneccesary
 
 protected:
 	double x, y, z;               // position
@@ -43,11 +50,38 @@ protected:
 
 class rectangularprism : public Shape{
 public:
-
+	rectangularprism(double xlength_, double ylength_, double zlength_)
+		: xlength(xlength_), ylength(ylength_), zlength(zlength_)
+	{}
 	void draw();
 	void setdimensions(double xlength_, double ylength_, double zlength_);
 protected:
 	double xlength, ylength, zlength;
+
+};
+
+class triangularprism : public Shape {
+public:
+	triangularprism(double base_, double height_, double offset_, double length_)//constructor
+		:base(base_), height(height_), offset(offset_), length(length_)
+	{}
+	void draw();
+	void setdimensions(double base_, double height_, double offset_, double length_);
+protected:
+	double base, height, offset, length;//offset is the distance along the x axis of the top point from the starting point
+};
+
+class trapezoid : public Shape {
+public:
+	trapezoid(double base_, double height_, double offset_, double length_, double topwidth_)
+		:base(base_), height(height_), offset(offset_), length(length_), topwidth(topwidth_)
+	{}
+	void draw();
+	void setdimensions(double base_, double height_, double offset_, double length_, double topwidth_);
+
+
+protected:
+	double base, height, offset, length, topwidth;//offset is the distance along the x axis of the top point from the starting point
 
 };
 #endif // for MTRN3500_SHAPE_H
